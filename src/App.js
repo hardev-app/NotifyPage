ea29f20d1452bfc1166f4f7ba359de0dbd83b756
hardev-app <hardev.app@gmail.com>
2023-12-14 22:33:46 -0600

    Ajustes 8

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import TermsAndConditions from './pages/TermsAndConditions';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import NoPage from "./pages/NoPage";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<TermsAndConditions />} />
+          <Route path="contact" element={<PrivacyPolicy />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/TermsAndConditions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TermsAndConditions = () => {
+  return (
+    <div>
+      <h2>Términos y Condiciones</h2>
+    </div>
+  );
+}
+
+export default TermsAndConditions;
