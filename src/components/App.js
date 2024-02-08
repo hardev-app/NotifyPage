65fd95b0ab4c7cb064b506b7001897a2a6631ad2
hardev-app <hardev.app@gmail.com>
2024-02-08 11:09:52 -0600

    Ajustes de carga->lazy

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,30 @@
+import React, { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from "./Layout"
 import PathConstants from "../routes/pathConstants";
+
+/*import NotifyPug from "../routes/NotifyPug"
 import Home from "../routes/Home";
-import NoPage from "../routes/NoPage";
-import NotifyPug from "../routes/NotifyPug"
 import PrivacyPolicy from "../routes/PrivacyPolicy";
 import TermsAndConditions from "../routes/TermsAndConditions";
+import NoPage from "../routes/NoPage";*/
+
+const ComponentHome = lazy(() => import('../routes/Home'));
+const ComponentNotifyPug = lazy(() => import('../routes/NotifyPug'));
+const ComponentTermsAndConditions = lazy(() => import('../routes/TermsAndConditions'));
+const ComponentPrivacyPolicy = lazy(() => import('../routes/PrivacyPolicy'));
+const ComponentNoPage = lazy(() => import('../routes/NoPage'));
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path={PathConstants.NOTIFYPUG} element={<NotifyPug />} />
-          <Route path={PathConstants.PRIVACY} element={<PrivacyPolicy />} />
-          <Route path={PathConstants.TERMS} element={<TermsAndConditions />} />
-          <Route path="*" element={<NoPage />} />
+          <Route index element={<ComponentHome />} />
+          <Route path={PathConstants.NOTIFYPUG} element={<ComponentNotifyPug />} />
+          <Route path={PathConstants.PRIVACY} element={<ComponentPrivacyPolicy />} />
+          <Route path={PathConstants.TERMS} element={<ComponentTermsAndConditions />} />
+          <Route path="*" element={<ComponentNoPage />} />
         </Route>
       </Routes>
     </>
